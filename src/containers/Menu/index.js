d358f6edf84f4256824d211706add3f3f6a385ea
azize1999 <azizchatti00@gmail.com>
2025-06-22 21:28:14 +0200

    changer et corriger

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -5,41 +5,35 @@ import Logo from "../../components/Logo";
 import "./style.scss";
 
 const Menu = () => {
-  const scrollToSection = (id) => {
+  const scrollToSection = (e, id) => {
+    e.preventDefault(); // Empêche le comportement normal du lien
     const section = document.getElementById(id);
     if (section) {
-      section.scrollIntoView({ behavior: "smooth" }); // défilement doux
+      section.scrollIntoView({ behavior: "smooth" }); // Défilement fluide
     }
   };
 
   return (
-    // Retourne le JSX qui définit l'apparence du menu.
     <nav>
-      {/* Composant Logo affiché dans la barre de navigation */}
       <Logo />
       <ul>
-        {/* Liste des liens du menu */}
         <li>
-          <a href="#nos-services" onClick={(e) => scrollToSection(e, "#services")}>
-            {/* Lien vers la section "Nos services" avec un défilement fluide */}
+          <a href="#nos-services" onClick={(e) => scrollToSection(e, "services")}>
             Nos services
           </a>
         </li>
         <li>
-          <a href="#nos-realisations" onClick={(e) => scrollToSection(e, "#events")}>
-            {/* Lien vers la section "Nos réalisations" avec un défilement fluide */}
+          <a href="#nos-realisations" onClick={(e) => scrollToSection(e, "events")}>
             Nos réalisations
           </a>
         </li>
         <li>
-          <a href="#notre-equipe" onClick={(e) => scrollToSection(e, "#team")}>
-            {/* Lien vers la section "Notre équipe" avec un défilement fluide */}
+          <a href="#notre-equipe" onClick={(e) => scrollToSection(e, "team")}>
             Notre équipe
           </a>
         </li>
       </ul>
-      <Button title="contact" onClick={(e) => scrollToSection(e, "#contact")}>
-        {/* Bouton pour aller à la section "Contact" avec un défilement fluide */}
+      <Button title="contact" onClick={(e) => scrollToSection(e, "contact")}>
         Contact
       </Button>
     </nav>
